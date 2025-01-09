9617f9e9e4dd203e0b14f5882ed838192c1f6917
dab7745 <dannybodner@dlbproduction.com>
2025-01-08 20:42:50 -0800

    who cares

--- scanner_project/settings.py
+++ scanner_project/settings.py
@@ -89,6 +89,18 @@ DATABASES = {
     }
 }
 
+if os.getenv('DIGITALOCEAN_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
