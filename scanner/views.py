c32b0de51ba47084987d510e0b5ba167dcb69ed4
dab7745 <dannybodner@dlbproduction.com>
2025-01-18 17:06:42 -0800

    who cares

--- scanner/views.py
+++ scanner/views.py
@@ -2274,12 +2274,32 @@ def check_trigger(symbol):
     for x in range(len(metrics)):
 
         # trigger to test
+        '''
         if (
             metrics[x].rolling_relative_volume > 1.2 and
             metrics[x].five_min_relative_volume > 1.3 and
             metrics[x].twenty_min_relative_volume > 2.0 and
             metrics[x].price_change_5min > 1
         ):
+        '''
+        
+        if (
+            #metrics[x].volume_24h_growth >= 10 and
+            metrics[x].rolling_relative_volume >= 1.3 and
+            metric.price_change_5min >= 0.75 and
+            metric.price_change_10min >= 1.5 and
+            metric.price_change_1hr <= -2 and
+            #metric.market_cap_growth_10min >= 0.5 and
+            3 <= metric.price_change_24hr <= 10 and
+            0.8 <= metric.daily_relative_volume <= 1.5 and
+            abs(metric.five_min_relative_volume - metric.twenty_min_relative_volume) <= 0.1
+            #abs(metric.last_price - metric.recent_local_low) <= 0.01 * metric.recent_local_low
+        ):
+
+
+
+
+
 
             print("------------------")
             print(coin.symbol)
