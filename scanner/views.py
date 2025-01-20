4851940ba319da4a3fd389bf7bf100b4a6049811
dab7745 <dannybodner@dlbproduction.com>
2025-01-19 22:14:02 -0800

    who cares

--- scanner/views.py
+++ scanner/views.py
@@ -7612,14 +7612,14 @@ def download_file(request, filename):
 
 
 
-
+# used to test out a trigger combination against the data we have in the db
 def check_trigger(symbol):
 
-    coin = Coin.objects.get(symbol=symbol)
-    metrics = Metrics.objects.filter(coin=coin).order_by('timestamp')
+    #coin = Coin.objects.get(symbol=symbol)
 
-    for x in range(len(metrics)):
+    coins = Coin.objects.all()
 
+<<<<<<< HEAD
         # trigger to test
         if (
             metrics[x].rolling_relative_volume > 1.2 and
@@ -7658,9 +7658,48 @@ def check_trigger(symbol):
             for y in range(x + 1, len(metrics)):
 
                 print(metrics[y].last_price)
+=======
+    for coin in coins:
+>>>>>>> 07b56bd (who cares)
+
+        metrics = Metrics.objects.filter(coin=coin).order_by('timestamp')
+
+        for x in range(len(metrics)):
+
+            # trigger to test
+            '''
+            if (
+                metrics[x].rolling_relative_volume > 1.2 and
+                metrics[x].five_min_relative_volume > 1.3 and
+                metrics[x].twenty_min_relative_volume > 2.0 and
+                metrics[x].price_change_5min > 1
+            ):
+            '''
+
+            if (
+                #metrics[x].volume_24h_growth >= 10 and
+                metrics[x].rolling_relative_volume >= 1.3 and
+                metrics[x].price_change_5min >= 0.75 and
+                metrics[x].price_change_10min >= 1.5 and
+                metrics[x].price_change_1hr <= -2 and
+                #metric.market_cap_growth_10min >= 0.5 and
+                3 <= metrics[x].price_change_24hr <= 10 and
+                0.8 <= metrics[x].daily_relative_volume <= 1.5 and
+                abs(metrics[x].five_min_relative_volume - metrics[x].twenty_min_relative_volume) <= 0.1
+                #abs(metric.last_price - metric.recent_local_low) <= 0.01 * metric.recent_local_low
+            ):
+
+                print("------------------")
+                print(coin.symbol)
+                print(metrics[x].timestamp)
+                print(metrics[x].last_price)
+
+                for y in range(x + 1, len(metrics)):
+
+                    print(metrics[y].last_price)
 
 
-
+# used to view coins and then delete the unneccessary ones
 def print_coins():
 
     coins = Coin.objects.filter(market_cap_rank__gt=200)
@@ -7739,6 +7778,17 @@ def index(request):
 
 
         # TRIGGER INFORMATION HERE...
+
+
+
+
+
+
+
+
+
+
+
 
 
         top_cryptos.append({
