668611b2d49efcefeb2b7ad44e905aa2853c1afd
dab7745 <dannybodner@dlbproduction.com>
2025-02-05 10:41:38 -0800

    who cares

--- scanner/management/commands/find_best_trigger.py
+++ scanner/management/commands/find_best_trigger.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from datetime import datetime
 import logging
 
-from scanner.views import finn, thirty_min_pattern_check, pattern_recognition, daily_high_low_data, create_main_csv, find_best_trigger, brute_force, brute_force_one, brute_force_short
+from scanner.views import finn, five_min_pattern_check, thirty_min_pattern_check, pattern_recognition, daily_high_low_data, create_main_csv, find_best_trigger, brute_force, brute_force_one, brute_force_short
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +17,9 @@ class Command(BaseCommand):
         #brute_force_one()
         #brute_force_short()
         #create_main_csv()
-        finn()
+        #finn()
         #daily_high_low_data()
         #pattern_recognition()
         #thirty_min_pattern_check()
+        five_min_pattern_check()
         logger.info("Done.")
