a41f509d0aaf24ab05d1f5e6dbd6f92f82a3220d
dab7745 <dannybodner@dlbproduction.com>
2025-03-25 17:10:44 -0700

    who cares

--- scanner/management/commands/link_entry_metrics.py
+++ scanner/management/commands/link_entry_metrics.py
@@ -7,10 +7,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         linked = 0
+
         for move in SuccessfulMove.objects.all():
+            if not move.timestamp:
+                continue
+
             closest = Metrics.objects.filter(
                 coin=move.coin,
-                timestamp__range=(move.entry_time - timedelta(minutes=10), move.entry_time + timedelta(minutes=10))
+                timestamp__range=(
+                    move.timestamp - timedelta(minutes=10),
+                    move.timestamp + timedelta(minutes=10)
+                )
             ).order_by('timestamp').first()
 
             if closest:
