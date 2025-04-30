12ba9d3c1484d5a220eb28501743f6e1ab68109d
dab7745 <dannybodner@dlbproduction.com>
2025-04-30 12:03:21 -0700

    who cares

--- scanner/management/commands/parquet_export_short.py
+++ scanner/management/commands/parquet_export_short.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         qs = RickisMetrics.objects.filter(
             short_result__isnull=False
         ).values(
-            'price', 'volume', 'change_5m', 'change_1h', 'change_24h',
+            'price', 'volume', 'change_1h', 'change_24h',
             'high_24h', 'low_24h', 'avg_volume_1h', 'relative_volume',
             'sma_5', 'sma_20', 'ema_12', 'ema_26', 'macd', 'macd_signal',
             'rsi', 'stochastic_k', 'stochastic_d', 'support_level',
