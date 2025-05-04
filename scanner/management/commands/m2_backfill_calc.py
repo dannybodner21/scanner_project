23d0e07983e1ee86e35c7f2c628bc6e1b39202fc
dab7745 <dannybodner@dlbproduction.com>
2025-05-03 21:13:05 -0700

    who cares

--- scanner/management/commands/m2_backfill_calc.py
+++ scanner/management/commands/m2_backfill_calc.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         start = make_aware(datetime(2025, 3, 22))
-        end = make_aware(datetime(2025, 5, 3))
+        end = make_aware(datetime(2025, 4, 13))
 
         metrics = RickisMetrics.objects.filter(
             timestamp__gte=start,
