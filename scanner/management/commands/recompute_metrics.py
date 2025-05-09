1c27ef97eb4a23300a49c012332c4b959cb61d80
dab7745 <dannybodner@dlbproduction.com>
2025-05-08 20:09:33 -0700

    who cares

--- scanner/management/commands/recompute_metrics.py
+++ scanner/management/commands/recompute_metrics.py
@@ -86,6 +86,7 @@ class Command(BaseCommand):
             if len(to_update) >= self.BATCH_SIZE:
                 RickisMetrics.objects.bulk_update(to_update, fields)
                 to_update.clear()
+                self.stdout.write("batch complete")
 
         # Final bulk update of any remaining records
         if to_update:
