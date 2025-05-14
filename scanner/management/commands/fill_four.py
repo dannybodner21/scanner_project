d55ef71a20aba5d420d9f735a9414a131aa943da
dab7745 <dannybodner@dlbproduction.com>
2025-05-14 12:07:11 -0700

    who cares

--- scanner/management/commands/fill_four.py
+++ scanner/management/commands/fill_four.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
             updated = False
 
             try:
-                if metric.rsi == 0:
+                if metric.rsi == 0 or metric.rsi == None:
                     rsi = calculate_rsi(coin, timestamp)
                     if rsi is not None:
                         metric.rsi = rsi
@@ -33,7 +33,7 @@ class Command(BaseCommand):
                     else:
                         print(f"rsi returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.macd == 0 or metric.macd_signal == 0:
+                if metric.macd == 0 or metric.macd_signal == 0 or metric.macd == None or metric.macd_signal == None:
                     macd, signal = calculate_macd(coin, timestamp)
                     if macd is not None and signal is not None:
                         metric.macd = macd
@@ -42,7 +42,7 @@ class Command(BaseCommand):
                     else:
                         print(f"macd returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.stochastic_k == 0 or metric.stochastic_d == 0:
+                if metric.stochastic_k == 0 or metric.stochastic_d == 0 or metric.stochastic_k == None or metric.stochastic_d == None:
                     k, d = calculate_stochastic(coin, timestamp)
                     if k is not None and d is not None:
                         metric.stochastic_k = k
@@ -51,7 +51,7 @@ class Command(BaseCommand):
                     else:
                         print(f"stochastic returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.support_level == 0 or metric.resistance_level == 0:
+                if metric.support_level == 0 or metric.resistance_level == 0 or metric.support_level == None or metric.resistance_level == None:
                     support, resistance = calculate_support_resistance(coin, timestamp)
                     if support is not None and resistance is not None:
                         metric.support_level = support
@@ -60,7 +60,7 @@ class Command(BaseCommand):
                     else:
                         print(f"support returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.sma_5 == 0:
+                if metric.sma_5 == 0 or metric.sma_5 == None:
                     sma5 = calculate_sma(coin, timestamp, 5)
                     if sma5 is not None:
                         metric.sma_5 = sma5
@@ -68,7 +68,7 @@ class Command(BaseCommand):
                     else:
                         print(f"sma5 returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.sma_20 == 0:
+                if metric.sma_20 == 0 or metric.sma_20 == None:
                     sma20 = calculate_sma(coin, timestamp, 20)
                     if sma20 is not None:
                         metric.sma_20 = sma20
@@ -76,7 +76,7 @@ class Command(BaseCommand):
                     else:
                         print(f"sma20 returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.stddev_1h == 0:
+                if metric.stddev_1h == 0 or metric.stddev_1h == None:
                     stddev = calculate_stddev_1h(coin, timestamp)
                     if stddev is not None:
                         metric.stddev_1h = stddev
@@ -84,7 +84,7 @@ class Command(BaseCommand):
                     else:
                         print(f"stddev returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.atr_1h == 0:
+                if metric.atr_1h == 0 or metric.atr_1h == None:
                     atr = calculate_atr_1h(coin, timestamp)
                     if atr is not None:
                         metric.atr_1h = atr
@@ -92,7 +92,7 @@ class Command(BaseCommand):
                     else:
                         print(f"atr1h returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.change_5m == 0:
+                if metric.change_5m == 0 or metric.change_5m == None:
                     change5m = calculate_price_change_five_min(coin)
                     if change5m is not None:
                         metric.change_5m = change5m
@@ -100,7 +100,7 @@ class Command(BaseCommand):
                     else:
                         print(f"change5 returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.change_since_high == 0:
+                if metric.change_since_high == 0 or metric.change_since_high == None:
                     high_diff = calculate_change_since_high(float(metric.price), float(metric.high_24h))
                     if high_diff is not None:
                         metric.change_since_high = high_diff
@@ -108,7 +108,7 @@ class Command(BaseCommand):
                     else:
                         print(f"change high returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.change_since_low == 0:
+                if metric.change_since_low == 0 or metric.change_since_low == None:
                     low_diff = calculate_change_since_low(float(metric.price), float(metric.low_24h))
                     if low_diff is not None:
                         metric.change_since_low = low_diff
@@ -116,7 +116,7 @@ class Command(BaseCommand):
                     else:
                         print(f"change low returned NONE: {coin.symbol} at {timestamp}")
 
-                if metric.obv == 0:
+                if metric.obv == 0 or metric.obv == None:
                     obv = calculate_obv(coin)
                     if obv is not None:
                         metric.obv = obv
