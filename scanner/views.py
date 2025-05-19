5814b800d0fd54593cf9fdc8a45800ea1bc858b0
dab7745 <dannybodner@dlbproduction.com>
2025-05-19 10:36:38 -0700

    who cares

--- scanner/views.py
+++ scanner/views.py
@@ -499,7 +499,7 @@ def get_model_results(request):
 # new model functions ----------------------------------------------------------
 
 PROJECT_ID = 'bodner-main-project'
-ENDPOINT_ID = '8508061657660915712'
+ENDPOINT_ID = '7813451783883194368'
 REGION = 'us-central1'
 
 SHORT_PROJECT_ID = 'bodner-main-project'
